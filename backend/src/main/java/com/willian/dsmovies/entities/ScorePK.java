f777cacff28539d759be6192abb05b7e473ef837
Willian Volpato <willian.volpato@gmail.com>
2022-03-10 20:16:02 -0300

    Create domain model and database seed

---
+++ backend/src/main/java/com/willian/dsmovies/entities/ScorePK.java
@@ -0,0 +1,38 @@
+package com.willian.dsmovies.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class ScorePK implements Serializable {
+
+	private static final long serialVersionUID = -6003951542588435419L;
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	public ScorePK() {}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
